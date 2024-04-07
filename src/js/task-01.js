d06bbe5a1c5a778278cfe0b639e85a02b00fc5cb
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-07 18:57:46 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,24 +1,11 @@
 const list = document.querySelector('ul');
-const sumCategories = list.children.length;
-console.log(`Number of categories: ${sumCategories}`);
+console.log(list);
 
-// const list = document.querySelector('ul');
-// console.log(list);
-
-// const items = list.children;
-// console.log(items);
-
-// const sum = items => {
-//   items.forEach(item => {
-//     console.log(`Number of categories: ${item}`);
-//   });
-// };
+const items = list.children;
+console.log(items);
 
 const title = document.querySelectorAll('.item h2');
 console.log(title);
-
-const child = document.querySelectorAll('.item ul li');
-console.log(child);
 
 title.forEach(el => {
   console.log(`Category: ${el.textContent}`);
