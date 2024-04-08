134e38fd243e5741a9b407f66f61f9c5b3f0ac11
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-08 18:59:37 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -6,7 +6,7 @@ const list = document.querySelector('ul#categories');
 const items = list.children;
 // console.log(items);
 
-console.log(`Number of categories: ${items.length}`);
+// console.log(`Number of categories: ${items.length}`);
 
 const title = document.querySelectorAll('.item h2');
 // console.log(title);
