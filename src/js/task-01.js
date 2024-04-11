82c51cbd3f6716087b424e7a7571b3eff8d145ab
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-11 16:46:16 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,40 +1,5 @@
 console.log('Execise-01');
 
-// /*
-//  * First exercise
-//  */
-
-// const countCategory = () => {
-//   const itemEl = document.querySelectorAll('.item');
-//   let numbers = 0;
-
-//   itemEl.forEach(() => {
-//     numbers += 1;
-//   });
-
-//   return `Number of categories: ${numbers}`;
-// };
-
-// console.log(countCategory());
-
-// /*
-//  * Second exercise
-//  */
-
-// const countNumbersOfItems = () => {
-//   const itemEl = document.querySelectorAll('.item li');
-//   const titleEl = document.querySelectorAll('h2');
-//   let numbers = 0;
-
-//   titleEl.forEach(item => {
-//     console.log(`Category: ${item.textContent}`);
-//     numbers += 1;
-//     console.log(`Elements: ${numbers}`);
-//   });
-// };
-
-// console.log(countNumbersOfItems());
-
 const categoryRef = document.querySelectorAll('.item');
 console.log(`Number of categories: ${categoryRef.length}`);
 
