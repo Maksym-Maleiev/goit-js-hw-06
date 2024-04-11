0826713f2b8970948d99da9790fed05396b959dc
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-11 14:57:51 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,29 +1,5 @@
 console.log('Execise-01');
 
-// const sum = items => {
-//   let numbers = 0;
-
-//   for (let i = 0; i < items.length; i += 1) {
-//     numbers += 1;
-//   }
-
-//   return `Number of categories: ${numbers}`;
-// };
-
-// console.log(sum(list.children));
-
-/*
- * Third method
- */
-// const items = document.querySelectorAll('.item');
-// let numbers = 0;
-
-// items.forEach(() => {
-//   numbers += 1;
-// });
-
-// console.log(`Number of categories: ${numbers}`);
-
 const countCategory = () => {
   const itemEl = document.querySelectorAll('.item');
   let numbers = 0;
