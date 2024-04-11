529aa62bc150c6c686d38e2926bdd7f8d597ec6a
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-11 17:40:43 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -20,38 +20,3 @@ const elementRef = ingredients.map(el => {
 });
 
 listRef.append(...elementRef);
-
-// const firstItemRef = document.createElement('li');
-// firstItemRef.textContent = 'Potatoes';
-// firstItemRef.classList.add('item');
-
-// const secondItemRef = document.createElement('li');
-// secondItemRef.textContent = 'Mushrooms';
-// secondItemRef.classList.add('item');
-
-// const thirdItemRef = document.createElement('li');
-// thirdItemRef.textContent = 'Garlic';
-// thirdItemRef.classList.add('item');
-
-// const fourthItemRef = document.createElement('li');
-// fourthItemRef.textContent = 'Tomatos';
-// fourthItemRef.classList.add('item');
-
-// const fifthItemRef = document.createElement('li');
-// fifthItemRef.textContent = 'Herbs';
-// fifthItemRef.classList.add('item');
-
-// const lastItemRef = document.createElement('li');
-// lastItemRef.textContent = 'Condiments';
-// lastItemRef.classList.add('item');
-
-// listRef.append(
-//   firstItemRef,
-//   secondItemRef,
-//   thirdItemRef,
-//   fourthItemRef,
-//   fifthItemRef,
-//   lastItemRef
-// );
-
-// console.log(listRef);
