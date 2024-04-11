c3f453146c310d8938a0554e37c179281fd52bb2
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-11 17:28:59 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -10,8 +10,38 @@ const ingredients = [
 ];
 
 const listRef = document.querySelector('ul#ingredients');
-const itemRef = document.createElement('li');
-itemRef.textContent = ingredients;
-itemRef.classList.add('item');
-listRef.append(itemRef);
+
+const firstItemRef = document.createElement('li');
+firstItemRef.textContent = 'Potatoes';
+firstItemRef.classList.add('item');
+
+const secondItemRef = document.createElement('li');
+secondItemRef.textContent = 'Mushrooms';
+secondItemRef.classList.add('item');
+
+const thirdItemRef = document.createElement('li');
+thirdItemRef.textContent = 'Garlic';
+thirdItemRef.classList.add('item');
+
+const fourthItemRef = document.createElement('li');
+fourthItemRef.textContent = 'Tomatos';
+fourthItemRef.classList.add('item');
+
+const fifthItemRef = document.createElement('li');
+fifthItemRef.textContent = 'Herbs';
+fifthItemRef.classList.add('item');
+
+const lastItemRef = document.createElement('li');
+lastItemRef.textContent = 'Condiments';
+lastItemRef.classList.add('item');
+
+listRef.append(
+  firstItemRef,
+  secondItemRef,
+  thirdItemRef,
+  fourthItemRef,
+  fifthItemRef,
+  lastItemRef
+);
+
 console.log(listRef);
