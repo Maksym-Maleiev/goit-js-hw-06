13cfa00e504c70fc6032b4c54335a9d8e601fc59
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-15 11:25:41 +0300

    Update task-04.js

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -3,5 +3,5 @@ console.log('Exercise_04');
 const btnDecrement = document.querySelector('button[data-action="decrement"]');
 console.log(btnDecrement);
 
-const btnDecrement = document.querySelector('button[data-action="decrement"]');
+const btnIncrement = document.querySelector('button[data-action="increment"]');
 console.log(btnDecrement);
