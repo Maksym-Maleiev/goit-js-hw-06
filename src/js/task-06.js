78436b328562ec8c658c4d6fec1665c5c10b885f
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-19 11:55:33 +0300

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,8 +1,8 @@
 console.log('Exercise_6');
 
-const inputRef = document.querySelector('#validation-input');
+const textInputRef = document.querySelector('#validation-input');
 // console.log(inputRef);
 
 const onChangeInput = e => {};
 
-inputRef.addEventListener('blur', onChangeInput);
+textInputRef.addEventListener('blur', onChangeInput);
