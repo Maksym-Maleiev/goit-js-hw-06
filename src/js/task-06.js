ead32d035bdf95056618db4a15e47b11e767e335
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-19 11:52:54 +0300

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -3,4 +3,6 @@ console.log('Exercise_6');
 const inputRef = document.querySelector('#validation-input');
 // console.log(inputRef);
 
+const onChangeInput = e => {};
+
 inputRef.addEventListener('input');
