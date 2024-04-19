f34c83eb24963acb4bcfda5313b9171a9841b4d7
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-19 12:02:31 +0300

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -4,6 +4,9 @@ const refs = {
   textInputRef: document.querySelector('#validation-input'),
 };
 
-const onChangeInput = e => {};
+const onChangeInput = e => {
+  if (e.currentTarget.lenght > 6) {
+  }
+};
 
 refs.textInputRef.addEventListener('input', onChangeInput);
