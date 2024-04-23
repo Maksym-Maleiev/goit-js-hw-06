0b0e123db73f38096b9835ca9fb2fc11ff592c54
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-23 14:34:09 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -3,6 +3,8 @@ console.log('Exercise_8');
 const formRef = document.querySelector('.login-form');
 // console.log(formRef);
 
-function onSubmit(e) {}
+function onSubmit(e) {
+  preventDefault();
+}
 
 formRef.addEventListener('submit', onSubmit);
