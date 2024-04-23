4dad084779a1c0d5461fadecf0a6937bef15bcbd
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-23 11:41:42 +0300

    Update task-07.js

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -3,8 +3,11 @@ console.log('Exercise_7');
 const inputRef = document.querySelector('#font-size-control');
 const spanRef = document.querySelector('#text');
 
+console.log(inputRef);
+console.log(spanRef);
+
 const onRangeChange = e => {
-  spanRef.textContent.style.fontSize = e.target.value;
+  spanRef.style.fontSize = e.target.value;
 };
 
 inputRef.addEventListener('input', onRangeChange);
