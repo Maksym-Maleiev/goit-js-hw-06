b01b7eee526c11ec7cd7d6947510291f6e2707f9
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-23 11:28:23 +0300

    Update task-07.js

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -8,7 +8,7 @@ console.log(refs.inputRef);
 console.log(refs.spanRef);
 
 const onRangeChange = e => {
-  refs.spanRef.style.fontSize = e.target.value;
+  refs.spanRef.textContent = e.target.value;
   console.log(e.target.value);
 };
 
