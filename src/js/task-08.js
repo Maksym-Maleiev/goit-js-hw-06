8adf11eff90f4ea943fe0865e31ce88f198b076d
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-23 14:43:52 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -6,9 +6,14 @@ const formRef = document.querySelector('.login-form');
 function onSubmit(e) {
   preventDefault();
 
-  const form = e.target;
-  const login = form.elements.email.value;
-  const password = form.elements.password.value;
+  const form = {
+    login: e.target.elements.email.value,
+    password: e.target.elements.password.value,
+  };
+
+  // const form = e.target;
+  // const login = form.elements.email.value;
+  // const password = form.elements.password.value;
 
   if (login === '' || password === '') {
     return console.log('Необхідно заповнити всі поля');
