cf4eddd7832a14748f1ef56d4d549fe040eac44b
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-24 10:58:27 +0300

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,6 +1,8 @@
 console.log('Exercise_10');
 
-const refs = {};
+const refs = {
+  box: document.querySelector('.box'),
+};
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
