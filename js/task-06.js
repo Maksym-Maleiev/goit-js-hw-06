7fffde13e6116bbf50a21b5c42076eac5fa870ed
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-24 13:51:13 +0300

    Create task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,17 @@
+console.log('Exercise_6');
+
+const refs = {
+  textInputRef: document.querySelector('#validation-input'),
+};
+
+const onCheckInput = e => {
+  if (e.currentTarget.value.length < 6) {
+    refs.textInputRef.classList.add('invalid');
+    refs.textInputRef.classList.remove('valid');
+  } else {
+    refs.textInputRef.classList.add('valid');
+    refs.textInputRef.classList.remove('invalid');
+  }
+};
+
+refs.textInputRef.addEventListener('blur', onCheckInput);
