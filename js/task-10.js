a4ce240ff0ba5426b1831d703c44edd30c7bb8b6
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-24 13:51:24 +0300

    Create task-10.js

---
+++ js/task-10.js
@@ -0,0 +1,45 @@
+console.log('Exercise_10');
+
+const refs = {
+  control: document.querySelector('#controls input'),
+  creatBtn: document.querySelector('button[data-create]'),
+  removeBtn: document.querySelector('button[data-destroy]'),
+  box: document.querySelector('#boxes'),
+};
+
+let inputValue = 0;
+
+const onCountClick = e => {
+  inputValue = e.target.value;
+};
+
+let width = 30;
+let height = 30;
+
+const createBoxes = () => {
+  for (let i = 0; i < refs.control.value; i += 1) {
+    refs.box.insertAdjacentHTML(
+      'beforeend',
+      `<div style="width: ${width}px; height: ${height}px;
+   background-color: ${getRandomHexColor()};"></div >`
+    );
+
+    width += 10;
+    height += 10;
+  }
+};
+
+const destroyBoxes = () => {
+  refs.control.value = '';
+  refs.box.innerHTML = '';
+  width = 30;
+  height = 30;
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.control.addEventListener('input', onCountClick);
+refs.creatBtn.addEventListener('click', createBoxes);
+refs.removeBtn.addEventListener('click', destroyBoxes);
