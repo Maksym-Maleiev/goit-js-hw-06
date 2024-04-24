4ac4adc7450389ec8342329fb4baec1c3508f3b5
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-24 11:51:29 +0300

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -7,14 +7,18 @@ const refs = {
   box: document.querySelector('#boxes'),
 };
 
-const onCountChange = e => {
-  const { value } = e.target;
-  const arr = Array(Number(value))
-    .fill('')
-    .map(() => `<div class = 'item'></div>`);
-
-  refs.box.insertAdjacentHTML('beforeend', arr.join(''));
+const onCountClick = e => {
+  e.stopPropagation();
 };
+
+// const onCountChange = e => {
+//   const { value } = e.target;
+//   const arr = Array(Number(value))
+//     .fill('')
+//     .map(() => `<div class = 'item'></div>`);
+
+//   refs.box.insertAdjacentHTML('beforeend', arr.join(''));
+// };
 
 const onCreatBox = e => {};
 
@@ -24,6 +28,6 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-refs.control.addEventListener('input', onCountChange);
+refs.control.addEventListener('input', onCountClick);
 refs.creatBtn.addEventListener('click', onCreatBox);
 refs.removeBtn.addEventListener('click', onDeleteBox);
