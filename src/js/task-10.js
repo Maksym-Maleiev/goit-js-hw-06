8b9eb9e821455390d627f420de94ac27eecbaee0
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-04-24 11:04:43 +0300

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -10,4 +10,4 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-refs.control.addEventListener();
+refs.control.addEventListener('input', onCreatBox);
